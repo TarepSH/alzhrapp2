708d724113718711b2c42e5c02327100a02bdf8b
tarepsh <tarepsh@gmail.com>
2021-08-18 12:56:53 -0700

    fixing link in navbar and autortion user to edite anything

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action  :authenticate_teacher!
+
     before_action do
         I18n.locale = :ar # Or whatever logic you use to choose.
-        :authenticate_teacher! 
       end
     
 end
