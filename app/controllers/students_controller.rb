7022c7fb73d11c0a31336e02788d316a2fafabbe
tarepsh <tarepsh@gmail.com>
2021-09-02 09:59:46 -0700

    showing only users for current_teacher.Studentclass.students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,7 @@ class StudentsController < ApplicationController
   
   # GET /students or /students.json
   def index
-    @students = Student.all
+    @students = current_teacher.Studentclass.students
   end
 
   # GET /students/1 or /students/1.json
