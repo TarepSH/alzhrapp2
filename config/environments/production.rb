4abcda722ccc5030c6ff26f5efb75c0d4116965e
TarepSH <tarepsh@gmail.com>
2021-11-01 20:05:39 +0200

    make sure that the local is chagen

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,5 +118,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.i18n.available_locales = [:ar, :en]
+  #config.i18n.available_locales = [:ar, :en]
 end
