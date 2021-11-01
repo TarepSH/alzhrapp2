656151e21bda4a06bec8e2cb856cac794a8c525e
TarepSH <tarepsh@gmail.com>
2021-11-01 21:18:48 +0200

    trying to force en english for local

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,8 @@ module Alzhraapp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    
+    config.i18n.enforce_available_locales = false
     config.i18n.default_locale = :en 
   end
 end
