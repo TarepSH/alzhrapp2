f49662e36da7e357c163f4756b9512c2b6955461
TarepSH <tarepsh@gmail.com>
2021-11-05 21:37:14 +0200

    comment local conatino in applcaiont contorller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action  :authenticate_teacher!
 
-    before_action do
-        I18n.locale = :ar # Or whatever logic you use to choose.
-      end
+    #before_action do
+    #    I18n.locale = :ar # Or whatever logic you use to choose.
+    #  end
     
       
 end
